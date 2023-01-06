b4206cef3d9db04ffb98682446e845c407f25fbf
SR <610311761@qq.com>
2023-01-06 17:05:13 +0800

    完善项目

---
+++ src/Editor/Main.java
@@ -0,0 +1,23 @@
+package Editor;
+
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWindow inter = new FileWindow();
+        inter.pack();
+        // 设置关闭退出程序
+        inter.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        //设置窗口大小
+        inter.setBounds(200, 180,550,360);
+        inter.setVisible(true);
+
+    }
+}
